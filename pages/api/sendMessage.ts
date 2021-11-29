ee8b6ff6e018110f66d93435b297733ddb4125bb
fjordski <fordheacock@gmail.com>
2021-11-29 12:44:41 -0500

    chore(result): add result to response

--- pages/api/sendMessage.ts
+++ pages/api/sendMessage.ts
@@ -8,14 +8,17 @@ export default async function sendMessage(
   const { phone, message, date } = req.body;
 
   try {
-    await reminderQueue.enqueue(
+    const result = await reminderQueue.enqueue(
       { phone, message, date },
       {
         id: phone,
         runAt: new Date(date),
       }
     );
-    return res.json({ status: 200, data: { phone, message, date } });
+    return res.json({
+      status: 200,
+      data: { result, body: { phone, message, date } },
+    });
   } catch (err) {
     res.json({ status: 400, detail: err });
   }
