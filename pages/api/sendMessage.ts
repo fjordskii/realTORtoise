6c9270f0f94d869f905510323e4db6139ef7888c
fjordski <fordheacock@gmail.com>
2021-11-29 14:13:21 -0500

    update

--- pages/api/sendMessage.ts
+++ pages/api/sendMessage.ts
@@ -1,36 +1,24 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import twilio from 'twilio';
-import { SYSTEM_FROM_NUMBER } from './queues/sms';
+import reminderQueue from './queues/sms';
 
 export default async function sendMessage(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const { phone, message, date } = req.body;
-  const accountSid = <string>process.env.TWILIO_ACCOUNT_SID;
-  const token = <string>process.env.TWILIO_AUTH_TOKEN;
-
-  const client = twilio(accountSid, token);
 
   try {
-    client.messages
-      .create({
-        body: message,
-        from: SYSTEM_FROM_NUMBER,
-        to: phone,
-      })
-      .then((data) => console.log(data));
-    // const result = await reminderQueue.enqueue(
-    //   { phone, message, date },
-    //   {
-    //     id: phone,
-    //     runAt: new Date(date),
-    //   }
-    // );
-    // return res.json({
-    //   status: 200,
-    //   data: { result },
-    // });
+    const result = await reminderQueue.enqueue(
+      { phone, message, date },
+      {
+        id: phone,
+        runAt: new Date(date),
+      }
+    );
+    return res.json({
+      status: 200,
+      data: { result },
+    });
   } catch (err) {
     res.json({ status: 400, detail: err });
   }
